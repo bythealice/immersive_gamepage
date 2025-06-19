48916258106ec8c7f09ba4a580c34ec20701025e
Alice Ramalho <thaisalice@icloud.com>
2025-06-18 21:23:25 -0300

    feat: add Hero, About sections, and animated components with new assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import NavBar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
         <NavBar/>
+        <Hero />
+        <About />
     </main>
   );
 }
